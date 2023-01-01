1e550eeb09dce2c49057ec5109445a368014efc8
AminPainter <aminpainter786@gmail.com>
2023-01-01 14:49:40 +0530

    pagination,sorting,projection implemented and tour model changed to store geospatial data

--- src/controllers/factory.js
+++ src/controllers/factory.js
@@ -8,7 +8,25 @@ export const createOne = Model =>
 
 export const getAll = Model =>
   catchAsync(async (req, res, next) => {
-    const docs = await Model.find(req.query);
+    const queryObj = { ...req.query };
+    ['fields', 'limit', 'page', 'sort'].forEach(
+      param => delete queryObj[param]
+    );
+
+    const queryString = JSON.stringify(queryObj).replace(
+      /\b(gt|gte|lt|lte)\b/g,
+      match => `$${match}`
+    );
+    let query = Model.find(JSON.parse(queryString));
+
+    if (req.query.sort) query = query.sort(req.query.sort.split(',').join(' '));
+    else query = query.sort('-createdAt');
+
+    if (req.query.fields)
+      query = query.select(req.query.fields.split(',').join(' '));
+    else query = query.select('-__v');
+
+    const docs = await query;
     res.formatter.ok(docs, { results: docs.length });
   });
 
--- src/models/tour.model.js
+++ src/models/tour.model.js
@@ -1,7 +1,22 @@
 import mongoose from 'mongoose';
 import slugify from 'slugify';
 
-const schema = mongoose.Schema(
+const pointSchema = new mongoose.Schema({
+  description: String,
+  type: {
+    type: String,
+    enum: ['Point'],
+    default: 'Point',
+  },
+  coordinates: {
+    type: [Number],
+    required: [true, 'A tour must have coordinates for its location fields'],
+  },
+  address: String,
+  day: Number,
+});
+
+const tourSchema = new mongoose.Schema(
   {
     name: {
       type: String,
@@ -41,6 +56,8 @@ const schema = mongoose.Schema(
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -62,6 +79,11 @@ const schema = mongoose.Schema(
       type: [mongoose.Types.ObjectId],
       ref: 'User',
     },
+    startLocation: {
+      type: pointSchema,
+      required: [true, 'A tour must have a start location'],
+    },
+    locations: [pointSchema],
     slug: String,
   },
   {
@@ -71,10 +93,10 @@ const schema = mongoose.Schema(
   }
 );
 
-schema.pre('save', function (next) {
+tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
 
-const Tour = mongoose.model('Tour', schema);
+const Tour = mongoose.model('Tour', tourSchema);
 export default Tour;
